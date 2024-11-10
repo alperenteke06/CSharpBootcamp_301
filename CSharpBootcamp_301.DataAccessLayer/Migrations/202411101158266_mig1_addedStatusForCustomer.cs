9d2d52a88d192bf42a7918fe6e9fe5dcd960a2e7
Your Name <you@example.com>
2024-11-10 15:24:56 +0300

    Migration İşlemleri ve Abstract Interface'lerin Hazırlanması

---
+++ CSharpBootcamp_301.DataAccessLayer/Migrations/202411101158266_mig1_addedStatusForCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpBootcamp_301.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1_addedStatusForCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "CustomerStatus", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "CustomerStatus");
+        }
+    }
+}
---
+++ CSharpBootcamp_301.DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpBootcamp_301.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CSharpBootcamp_301.DataAccessLayer.Context.BootcampContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(CSharpBootcamp_301.DataAccessLayer.Context.BootcampContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
