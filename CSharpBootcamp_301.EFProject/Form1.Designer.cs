9d2ce107c7cfc5a2b7080a17d9fa58bd21618277
Your Name <you@example.com>
2024-11-17 00:36:25 +0300

    Yeni Ders İçeriği - Entity Framework Metotları - Proje Uygulaması

--- CSharpBootcamp_301.EFProject/Form1.Designer.cs
+++ CSharpBootcamp_301.EFProject/Form1.Designer.cs
@@ -28,13 +28,175 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtGuideID = new System.Windows.Forms.TextBox();
+			this.btnListGuiders = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.txtGuideName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtGuideSurname = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnAddGuider = new System.Windows.Forms.Button();
+			this.btnDeleteGuider = new System.Windows.Forms.Button();
+			this.btnUpdateGuider = new System.Windows.Forms.Button();
+			this.btnListGuiderByID = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(30, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(87, 24);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Guide ID:";
+			// 
+			// txtGuideID
+			// 
+			this.txtGuideID.Location = new System.Drawing.Point(123, 12);
+			this.txtGuideID.Name = "txtGuideID";
+			this.txtGuideID.Size = new System.Drawing.Size(296, 32);
+			this.txtGuideID.TabIndex = 1;
+			// 
+			// btnListGuiders
+			// 
+			this.btnListGuiders.BackColor = System.Drawing.Color.Cyan;
+			this.btnListGuiders.Location = new System.Drawing.Point(123, 270);
+			this.btnListGuiders.Name = "btnListGuiders";
+			this.btnListGuiders.Size = new System.Drawing.Size(296, 42);
+			this.btnListGuiders.TabIndex = 2;
+			this.btnListGuiders.Text = "List";
+			this.btnListGuiders.UseVisualStyleBackColor = false;
+			this.btnListGuiders.Click += new System.EventHandler(this.btnListGuiders_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(485, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(661, 348);
+			this.dataGridView1.TabIndex = 3;
+			// 
+			// txtGuideName
+			// 
+			this.txtGuideName.Location = new System.Drawing.Point(123, 50);
+			this.txtGuideName.Name = "txtGuideName";
+			this.txtGuideName.Size = new System.Drawing.Size(296, 32);
+			this.txtGuideName.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(53, 53);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(64, 24);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Name:";
+			// 
+			// txtGuideSurname
+			// 
+			this.txtGuideSurname.Location = new System.Drawing.Point(123, 88);
+			this.txtGuideSurname.Name = "txtGuideSurname";
+			this.txtGuideSurname.Size = new System.Drawing.Size(296, 32);
+			this.txtGuideSurname.TabIndex = 7;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(30, 91);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(89, 24);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Surname:";
+			// 
+			// btnAddGuider
+			// 
+			this.btnAddGuider.BackColor = System.Drawing.Color.Lime;
+			this.btnAddGuider.Location = new System.Drawing.Point(123, 126);
+			this.btnAddGuider.Name = "btnAddGuider";
+			this.btnAddGuider.Size = new System.Drawing.Size(296, 42);
+			this.btnAddGuider.TabIndex = 8;
+			this.btnAddGuider.Text = "Add";
+			this.btnAddGuider.UseVisualStyleBackColor = false;
+			this.btnAddGuider.Click += new System.EventHandler(this.btnAddGuider_Click);
+			// 
+			// btnDeleteGuider
+			// 
+			this.btnDeleteGuider.BackColor = System.Drawing.Color.Red;
+			this.btnDeleteGuider.Location = new System.Drawing.Point(123, 174);
+			this.btnDeleteGuider.Name = "btnDeleteGuider";
+			this.btnDeleteGuider.Size = new System.Drawing.Size(296, 42);
+			this.btnDeleteGuider.TabIndex = 9;
+			this.btnDeleteGuider.Text = "Delete";
+			this.btnDeleteGuider.UseVisualStyleBackColor = false;
+			this.btnDeleteGuider.Click += new System.EventHandler(this.btnDeleteGuider_Click);
+			// 
+			// btnUpdateGuider
+			// 
+			this.btnUpdateGuider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.btnUpdateGuider.Location = new System.Drawing.Point(123, 222);
+			this.btnUpdateGuider.Name = "btnUpdateGuider";
+			this.btnUpdateGuider.Size = new System.Drawing.Size(296, 42);
+			this.btnUpdateGuider.TabIndex = 10;
+			this.btnUpdateGuider.Text = "Update";
+			this.btnUpdateGuider.UseVisualStyleBackColor = false;
+			this.btnUpdateGuider.Click += new System.EventHandler(this.btnUpdateGuider_Click);
+			// 
+			// btnListGuiderByID
+			// 
+			this.btnListGuiderByID.BackColor = System.Drawing.Color.Cyan;
+			this.btnListGuiderByID.Location = new System.Drawing.Point(123, 318);
+			this.btnListGuiderByID.Name = "btnListGuiderByID";
+			this.btnListGuiderByID.Size = new System.Drawing.Size(296, 42);
+			this.btnListGuiderByID.TabIndex = 11;
+			this.btnListGuiderByID.Text = "Fetch By ID";
+			this.btnListGuiderByID.UseVisualStyleBackColor = false;
+			this.btnListGuiderByID.Click += new System.EventHandler(this.btnListGuiderByID_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(1158, 376);
+			this.Controls.Add(this.btnListGuiderByID);
+			this.Controls.Add(this.btnUpdateGuider);
+			this.Controls.Add(this.btnDeleteGuider);
+			this.Controls.Add(this.btnAddGuider);
+			this.Controls.Add(this.txtGuideSurname);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtGuideName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.btnListGuiders);
+			this.Controls.Add(this.txtGuideID);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "Form1";
+			this.Text = "Guide Process";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtGuideID;
+		private System.Windows.Forms.Button btnListGuiders;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.TextBox txtGuideName;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtGuideSurname;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnAddGuider;
+		private System.Windows.Forms.Button btnDeleteGuider;
+		private System.Windows.Forms.Button btnUpdateGuider;
+		private System.Windows.Forms.Button btnListGuiderByID;
 	}
 }
 
--- CSharpBootcamp_301.EFProject/Form1.cs
+++ CSharpBootcamp_301.EFProject/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpBootcamp_301.EFProject.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,64 @@ namespace CSharpBootcamp_301.EFProject
 {
 	public partial class Form1 : Form
 	{
+
+		BootcampEFTravelDBEntities_new db = new BootcampEFTravelDBEntities_new();
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void btnListGuiders_Click(object sender, EventArgs e)
+		{
+			ListAndShow();
+		}
+
+		private void btnAddGuider_Click(object sender, EventArgs e)
+		{
+			Guide guider = new Guide();
+			guider.GuideName = txtGuideName.Text;
+			guider.GuideSurname = txtGuideSurname.Text;
+			db.Guides.Add(guider);
+			db.SaveChanges();
+			MessageBox.Show("Guide Added Succesfully", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			ListAndShow();
+		}
+
+		#region ListMethod
+		private void ListAndShow()
+		{
+			var values = db.Guides.ToList();
+			dataGridView1.DataSource = values;
+		}
+		#endregion
+
+		private void btnDeleteGuider_Click(object sender, EventArgs e)
+		{
+			int selectedGuiderID = int.Parse(txtGuideID.Text);
+			var removeGuider = db.Guides.Find(selectedGuiderID);
+			db.Guides.Remove(removeGuider);
+			db.SaveChanges();
+			MessageBox.Show("Guide Deleted Succesfully", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			ListAndShow();
+		}
+
+		private void btnUpdateGuider_Click(object sender, EventArgs e)
+		{
+			var selectedGuiderID = int.Parse(txtGuideID.Text);
+			var updateGuider = db.Guides.Find(selectedGuiderID);
+			updateGuider.GuideName = txtGuideName.Text;
+			updateGuider.GuideSurname = txtGuideSurname.Text;
+			db.SaveChanges();
+			MessageBox.Show("Guide Updated Succesfully", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			ListAndShow();
+		}
+
+		private void btnListGuiderByID_Click(object sender, EventArgs e)
+		{
+			int selectedGuiderID = int.Parse(txtGuideID.Text);
+			var getGuider = db.Guides.Where(x => x.GuideID == selectedGuiderID).ToList();
+			dataGridView1.DataSource = getGuider;
 		}
 	}
 }
